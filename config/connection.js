05647afdb95a20b5a8fee605db853c167611766f
loc-koan <loc.koan@yahoo.com>
2020-07-16 23:08:43 -0500

    added jawsdb and edited connection.js file

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 /* Set up MySQL connection */
 var mysql = require('mysql2');
+var connection;
 
-var connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'ClassCod3!',
-    database: 'burgers_db',
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'x40p5pp7n9rowyv6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        port: 3306,
+        user: 'lm9xqaxuuanfu5iy',
+        password: 'ysvpya2sevrcr8gy!',
+        database: 'fwg3uiyrzs3wv8ee'
+    });
+};
 
 /* Make connection */
 connection.connect(function (err) {
